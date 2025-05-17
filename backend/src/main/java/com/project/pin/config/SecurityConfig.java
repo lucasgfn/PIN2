bea67c7d343efc6a71e8286aa4b63fef916a127b
clarabecker <Claraeu15@outlook.com>
2025-05-17 19:24:38 -0300

    Add SecurityConfig for  user authentication

---
+++ backend/src/main/java/com/project/pin/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.project.pin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //permite todas as rotas por enquanto
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
