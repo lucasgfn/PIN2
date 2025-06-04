94c6115f329aa275ae2eba1107aa02f7b0488daa
clarabecker <Claraeu15@outlook.com>
2025-06-03 22:38:36 -0300

    Minor updates

--- frontend/src/components/forms/LoginForm.tsx
+++ frontend/src/components/forms/LoginForm.tsx
@@ -19,86 +19,110 @@ const LoginForm : React.FC = () => {
         navigate('/cadastro');
     }
 
-    return(
-        <>
-            <Container maxWidth="sm">
-                <Paper elevation={3} 
-                    sx={{
-                    p: 6, 
-                    mt: 8,
-                    borderRadius: 20,
-                    border: '2px solid',         
-                    borderColor: 'purple',
-    
-                    }}> 
-                <img src={logo} alt={"logo"} style={{marginLeft: "80px", width: "300px", height: "auto"}}/>
-                    <Box component="form" onSubmit={handleSubmit}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                            borderRadius: 3,
-                            '& fieldset': {
-                                borderColor: 'deeppink', 
-                              },
-                            },
-                            '& label.Mui-focused': {
-                                color: 'purple',
-                                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':{
-                                    borderColor: 'pink',
-                            },
-                        },
-                        }} >
-                        <TextField
-                            label="Username"
-                            type="text"
-                            fullWidth
-                            margin="normal"
-                            required
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                         <TextField className=""
-                            label="Senha"
-                            type="password"
-                            fullWidth
-                            margin="normal"
-                            required
-                            value={senha}
-                            onChange={(e) => setSenha(e.target.value)}
-                        />
+    return (
+      <>
+        <Box sx={{ backgroundColor: "#F5F5F5", minHeight: "50vh", py: 4 }}>
+          <Container maxWidth="sm">
+            <Paper
+              elevation={3}
+              sx={{
+                p: 6,
+                mt: 8,
+                borderRadius: 20,
+                border: "2px solid",
+                borderColor: "purple",
+                backgroundColor: "#F5F5F5",
+              }}
+            >
+              <img
+                src={logo}
+                alt={"logo"}
+                style={{ marginLeft: "80px", width: "300px", height: "auto" }}
+              />
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: 3,
+                    "& fieldset": {
+                      borderColor: "deeppink",
+                    },
+                  },
+                  "& label.Mui-focused": {
+                    color: "purple",
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                      {
+                        borderColor: "pink",
+                      },
+                  },
+                  backgroundColor: "#F5F5F5",
+                }}
+              >
+                <TextField
+                  label="Username"
+                  type="text"
+                  fullWidth
+                  margin="normal"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                  className=""
+                  label="Senha"
+                  type="password"
+                  fullWidth
+                  margin="normal"
+                  required
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                />
 
-                        <Box mt={3} display="flex" gap={2} justifyContent={"space-around"}> 
-                            <Button
-                                variant="contained"
-                                color="secondary"
-                                type="submit"
-                                sx={{ 
-                                    fontSize: '1.1rem',
-                                    px: 7, 
-                                    borderRadius: 3, 
-                                    backgroundColor: '#007BFF', 
-                                        '&:hover': { 
-                                            backgroundColor: 'deeppink' 
-                                        }}}
-                            > Entrar
-                            </Button>
-                            <Button
-                                variant="outlined"
-                                onClick={handleCadastrar}
-                                sx={{  
-                                    fontSize: '0.9rem',
-                                    px: 7,  
-                                    borderRadius: 3, 
-                                    color: '#000', 
-                                    borderColor: 'deeppink' }}
-                            > Cadastra-se
-                            </Button>
-                            </Box> 
-                         </Box>
-                </Paper>
-            </Container>
-            
-        </>
-    )
+                <Box
+                  mt={3}
+                  display="flex"
+                  gap={2}
+                  justifyContent={"space-around"}
+                >
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    type="submit"
+                    sx={{
+                      fontSize: "1.1rem",
+                      px: 7,
+                      borderRadius: 3,
+                      backgroundColor: "#007BFF",
+                      "&:hover": {
+                        backgroundColor: "deeppink",
+                      },
+                    }}
+                  >
+                    {" "}
+                    Entrar
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={handleCadastrar}
+                    sx={{
+                      fontSize: "0.9rem",
+                      px: 7,
+                      borderRadius: 3,
+                      color: "#000",
+                      borderColor: "deeppink",
+                    }}
+                  >
+                    {" "}
+                    Cadastra-se
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Container>
+        </Box>
+      </>
+    );
 
 
 }
